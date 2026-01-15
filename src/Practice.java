7a843918b424f83f14db8fa29444c335f6393a52
Lilian <210286008+liliann19@users.noreply.github.com>
2026-01-15 14:52:47 -0800

    created contains and remove methods

--- src/Practice.java
+++ src/Practice.java
@@ -6,7 +6,12 @@ public class Practice {
         ryan.next = new Node('t', null);
         ryan.next.next = new Node('x', null);
 
-        printList(head);
+        // Expect to print true
+        System.out.println(contains(head, 'x'));
+        //Expect to print false
+        System.out.println(contains(head, 'e'));
+
+        remove(head,'t');
     }
 
     public static void printList(Node start) {
@@ -16,5 +21,27 @@ public class Practice {
             System.out.println(current.value);
             current = current.next;
         }
+    }
+
+    public static boolean contains(Node start, char toFind) {
+        while (start != null) {
+            if (start.value == toFind) {
+                return true;
+            } start = start.next;
+        }
+        return false;
+    }
+
+    // remove the first node that has toRemove
+    public static char remove(Node head, char toRemove) {
+        Node current = head;
+        while (current.next != null) {
+            if (current.next.value == toRemove) {
+                current.next = current.next.next;
+                return toRemove;
+            }
+            current = current.next;
+        }
+        return '\0';
     }
 }
