0b4bdb028589342d99f11353b69ce585ee58f957
Lilian <210286008+liliann19@users.noreply.github.com>
2026-01-20 19:33:30 -0800

    updated constructor and implemented printBackwards method

--- src/MyLL.java
+++ src/MyLL.java
@@ -1,22 +1,24 @@
-public class MyLL {
-    private class Node {
-        public char value;
-        public Node next;
+public class MyLL<T> {
+    private class Node<E> {
+        public E value;
+        public Node<E> next;
+        public Node<E> prev;
 
-        public Node(char value, Node next) {
+        public Node(E value, Node<E> next) {
             this.value = value;
             this.next = next;
+            this.prev = null;
         }
     }
 
-    private Node head;
+    private Node<T> head;
 
     public MyLL() {
         head = null;
     }
 
     public void printList() {
-        Node current = head;
+        Node<T> current = head;
 
         while (current != null) {
             System.out.println(current.value);
@@ -24,11 +26,28 @@ public class MyLL {
         }
     }
 
-    public boolean contains(char toFind) {
-        Node current = head;
+    public void addToBack(T toAdd) {
+        Node<T> newNode = new Node<T>(toAdd, null);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node<T> current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+
+        current.next = newNode;
+        newNode.prev = current;
+    }
+
+    public boolean contains(T toFind) {
+        Node<T> current = head;
 
         while (current != null) {
-            if (current.value == toFind) {
+            if (current.value.equals(toFind)) {
                 return true;
             }
             current = current.next;
@@ -37,27 +56,42 @@ public class MyLL {
     }
 
     // remove the first node that has toRemove
-    public char remove(char toRemove) {
+    public T remove(T toRemove) {
 
         if (head == null) {
-            return '\0';
+            return null;
         }
 
-        if (head.value == toRemove) {
+        if (head.value.equals(toRemove)) {
             head = head.next;
             return toRemove;
-            
         }
 
-        Node current = head;
+        Node<T> current = head;
 
         while (current.next != null) {
-            if (current.next.value == toRemove) {
+            if (current.next.value.equals(toRemove)) {
                 current.next = current.next.next;
                 return toRemove;
             }
             current = current.next;
         }
-        return '\0';
+        return null;
+    }
+
+    public void printBackwards() {
+        Node<T> current = head;
+
+        // going forward
+        while (current.next != null){
+            current = current.next;
+        }
+
+        // going backwards 
+        while(current != null){
+            System.out.println(current.value);
+            current = current.prev;
+        }
+
     }
 }
--- src/Practice.java
+++ src/Practice.java
@@ -1,17 +1,43 @@
 public class Practice {
     public static void main(String[] args) {
-        Node head = new Node('g', null);
-        Node ryan = new Node('s', null);
-        head.next = ryan;
-        ryan.next = new Node('t', null);
-        ryan.next.next = new Node('x', null);
+        // Node head = new Node('g', null);
+        // Node ryan = new Node('s', null);
+        // head.next = ryan;
+        // ryan.next = new Node('t', null);
+        // ryan.next.next = new Node('x', null);
 
-        // Expect to print true
-        System.out.println(contains(head, 'x'));
-        //Expect to print false
-        System.out.println(contains(head, 'e'));
+        // // Expect to print true
+        // System.out.println(contains(head, 'x'));
+        // //Expect to print false
+        // System.out.println(contains(head, 'e'));
 
-        remove(head,'t');
+        // remove(head,'t');
+
+        MyLL<Character> list = new MyLL<>();
+        list.addToBack('x');
+        list.addToBack('m');
+        list.addToBack('v');
+        list.addToBack('q');
+
+        list.printList();
+
+        MyLL<Integer> intList = new MyLL<>();
+        intList.addToBack(5);
+        intList.addToBack(99);
+        intList.addToBack(2);
+        intList.addToBack(-33333);
+        intList.addToBack(66);
+
+        intList.printList();
+
+        MyLL<Integer> doubleIntList = new MyLL<>();
+        doubleIntList.addToBack(1);
+        doubleIntList.addToBack(2);
+        doubleIntList.addToBack(3);
+        doubleIntList.addToBack(4);
+        
+        doubleIntList.printBackwards();
+
     }
 
     public static void printList(Node start) {
